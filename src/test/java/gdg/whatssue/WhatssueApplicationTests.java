08712360d6b74acbc7882daed2905880de17cb10
kjyyjk920 <rlawnsdud920@gmail.com>
2023-09-04 01:02:21 +0900

    Init: initial commit

---
+++ src/test/java/gdg/whatssue/WhatssueApplicationTests.java
@@ -0,0 +1,13 @@
+package gdg.whatssue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
