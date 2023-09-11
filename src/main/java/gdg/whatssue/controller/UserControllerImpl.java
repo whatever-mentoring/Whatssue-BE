1621b9a77c2bd98d7393e23408a491010d132b6a
kjyyjk <rlawnsdud920@gmail.com>
2023-09-11 20:29:03 +0900

    Feat: 회원가입 및 로그인api 스켈레톤 코드

---
+++ src/main/java/gdg/whatssue/controller/UserControllerImpl.java
@@ -0,0 +1,26 @@
+package gdg.whatssue.controller;
+
+import gdg.whatssue.controller.inter.UserController;
+import jdk.jshell.spi.ExecutionControl.NotImplementedException;
+import org.springframework.http.ResponseEntity;
+
+public class UserControllerImpl implements UserController {
+
+    @Override
+    public ResponseEntity userSignUp() throws NotImplementedException {
+        //"TODO: This api should be implemented"
+        throw new NotImplementedException("hi");
+    }
+
+    @Override
+    public ResponseEntity checkIdDuplicate() throws NotImplementedException {
+        //"TODO: This api should be implemented"
+        throw new NotImplementedException("hi");
+    }
+
+    @Override
+    public ResponseEntity userLogin() throws NotImplementedException {
+        //"TODO: This api should be implemented"
+        throw new NotImplementedException("hi");
+    }
+}
---
+++ src/main/java/gdg/whatssue/controller/inter/UserController.java
@@ -0,0 +1,22 @@
+package gdg.whatssue.controller.inter;
+
+import jdk.jshell.spi.ExecutionControl.NotImplementedException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/user")
+public interface UserController {
+    
+    //회원가입
+    @PostMapping("/sign-up")
+    public ResponseEntity userSignUp() throws NotImplementedException;
+
+    //아이디 중복 체크
+    @PostMapping("/sign-up/checkId")
+    public ResponseEntity checkIdDuplicate() throws NotImplementedException;
+
+    // 로그인
+    @PostMapping("/login")
+    public ResponseEntity userLogin() throws NotImplementedException;
+}
