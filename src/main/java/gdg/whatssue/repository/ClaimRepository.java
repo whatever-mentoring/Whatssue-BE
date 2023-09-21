850c40688c2cff309a986a69f264cc094c8614f1
kim ye jin <108534554+dPwls0125@users.noreply.github.com>
2023-09-21 21:43:06 +0900

    [What 135] 정산 청구 post 기능  (#59)
    * feat : 회계 entity
    
    * feat : 정산 요청 api
    
    * jwt, security 관련 주석처리

---
+++ src/main/java/gdg/whatssue/repository/ClaimRepository.java
@@ -0,0 +1,10 @@
+package gdg.whatssue.repository;
+
+import gdg.whatssue.entity.Claim;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClaimRepository extends JpaRepository<Claim,Long> {
+
+}
---
+++ src/main/java/gdg/whatssue/service/AccountService.java
@@ -0,0 +1,38 @@
+package gdg.whatssue.service;
+
+import gdg.whatssue.entity.Claim;
+import gdg.whatssue.entity.Club;
+import gdg.whatssue.repository.ClaimRepository;
+import gdg.whatssue.repository.ClubRepository;
+import gdg.whatssue.service.dto.AccountClaimDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.math.BigDecimal;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AccountService {
+    private final ClaimRepository claimRepository;
+    private final ClubRepository clubRepository;
+     public ResponseEntity<?> createClaim(AccountClaimDto dto) throws Exception {
+         Long clubId = 1L; // 1로 가정
+         Club club = clubRepository.findById(clubId).orElseThrow(() -> (
+                 new ResponseStatusException(HttpStatus.NOT_FOUND, "클럽을 찾을 수 없습니다.")
+         ));
+            Claim claim = Claim.builder()
+                    .club(club)
+                    .isClosed(false)
+                    .claimDate(dto.getClaimDate())
+                    .claimAmount(new BigDecimal(dto.getClaimAmount()))
+                    .claimName(dto.getClaimName())
+                    .build();
+         claimRepository.save(claim);
+         return ResponseEntity.ok("정산 청구 완료");
+     }
+}
